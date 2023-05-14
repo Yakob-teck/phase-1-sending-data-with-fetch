d1640160d5a700d1141a6e680c444d48405e5d08
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-05-14 01:58:23 -0600

    sendingData

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Add your code here
+function submitData(name, email) {
+    const submitData = {
+      name: name,
+      email: email,
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(submitData),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Failed to submit data");
+        }
+      })
+      .then(function (data) {
+        const newId = data.id;
+        appendIdToDOM(newId);
+      })
+      .catch(function (error) {
+        appendErrorMessageToDOM(error.message);
+      });
+  }
+  
+  function appendIdToDOM(id) {
+    const idElement = document.createElement("p");
+    idElement.textContent = "New ID: " + id;
+    document.body.appendChild(idElement);
+  }
+  
+  function appendErrorMessageToDOM(message) {
+    const errorElement = document.createElement("p");
+    errorElement.textContent = message;
+    document.body.appendChild(errorElement);
+  }
+  
+  
